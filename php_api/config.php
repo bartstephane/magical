cd6515ec56960a5f6444a4f819bb9a2435eba5b8
bartstephane <contact@stefbart.ch>
2025-08-13 16:07:10 +0200

    PHP API config

---
+++ php_api/config.php
@@ -0,0 +1,9 @@
+<?php
+// Configuration MySQL
+define('DB_HOST', 'mgtw.myd.infomaniak.com'); // ou l'host Infomaniak
+define('DB_NAME', 'mgtw_magical');
+define('DB_USER', 'mgtw_magical');
+define('DB_PASS', 'RBlAD_R9VnCrTZCbnt3f');
+
+// Token d'API (à générer toi-même)
+define('API_TOKEN', 'jsso31Y#CS_m8jp$Kt09');

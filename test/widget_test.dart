c254ef83c10ba7c1a75c6c25ba5f9544197d9c70
bartstephane <contact@stefbart.ch>
2025-08-17 10:18:12 +0200

    Update widget test remove unused import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:magical/magical.dart';
 
-import 'package:magical/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
